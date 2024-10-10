bab6de1bdca80e713e813919481f9d10710eb866
Pedro Borges <oi@pedroborg.es>
2024-10-10 09:57:40 -0300

    Auto-generate trails routes

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import laravel from 'laravel-vite-plugin'
 import { defineConfig } from 'vite'
+import { run } from 'vite-plugin-run'
 
 export default defineConfig({
     plugins: [
@@ -9,6 +10,18 @@ export default defineConfig({
             ssr: 'resources/js/ssr.ts',
             refresh: true,
         }),
+        run([
+            {
+                name: 'trail generate routes',
+                pattern: 'routes/*.php',
+                run: ['php', 'artisan', 'trail:generate'],
+            },
+            {
+                name: 'clear compiled views',
+                pattern: 'routes/*.php',
+                run: ['php', 'artisan', 'view:clear'],
+            },
+        ]),
         svelte(),
     ],
 })
